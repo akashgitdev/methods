cb22e3890d6ef52dc1fe3bc2aebf8cbd279a675e
akashgitdev <nagaakashduddupudi1712@email.com>
2024-10-23 11:16:14 +0530

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var fruits=["apple","banana","orange"];
+     console.log(fruits.join(" ,"))
+
+var arr=[1,2,3,4,5];
+console.log(arr.join("-"))
+
+var arr=['h','e','l','l','0'];
+console.log(arr.join(""))
+
+var text="javascript is awesome"
+console.log(text.slice(0,10))
+
+var text="i love learning javascript"
+console.log(text.slice(7,15))
+
+var text="frontend development "
+console.log(text.slice(-12,-1))
+
+var date='17-12-2002';
+console.log(date.split("-"))
+
+var text="i love learning javascript"
+console.log(text.split(" ",2))
+
+var text="https://www.w3schools.com/python/"
+console.log(text.split("/"))
+
+
+
